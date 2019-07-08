0bf03ed28af90172974d7c1303c11a25ddd3c72a
Ehlen, David <david.ehlen@dm.de>
2019-07-08 11:09:16 +0200

    Initial Commit

---
+++ Capture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Capture
+//
+//  Created by Ehlen, David on 08.07.19.
+//  Copyright © 2019 David Ehlen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
